d8740a928c27b67fb56587916f3887b93a944c2f
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-10-26 13:29:41 +0300

    connections established and CRUD-functions written

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'trial-3a5b9',
+    appId: '1:215111771381:web:c95a3b604b50898a2211bc',
+    storageBucket: 'trial-3a5b9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBjlWy5t7drbjee89-SgAiMBN81LcSthUs',
+    authDomain: 'trial-3a5b9.firebaseapp.com',
+    messagingSenderId: '215111771381',
+  },
   production: false
 };
 
