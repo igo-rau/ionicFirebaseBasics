8c1784e58fe1cacc9914eb4f95c2da6a49134525
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-10-26 14:01:22 +0300

    addNote ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -7,9 +8,46 @@ import { DataService } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private dataService: DataService) {
+  notes = [];
+  constructor(private dataService: DataService, private alertCtrl: AlertController) {
     this.dataService.getNotes().subscribe((res) => {
       console.log(res);
+      this.notes = res;
     });
+  }
+
+  openNote(note) {
+    
+  }
+
+  async addNote() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Please enter the title',
+          type: 'text'
+        },
+        {
+          name: 'text',
+          placeholder: 'Enter your note here',
+          type: 'textarea'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: (res) => {
+            this.dataService.addNote({title: res.title, text: res.text});
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
